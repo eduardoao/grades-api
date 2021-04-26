ca8774b7a6e699a337f9a2033283014d04912ed6
Eduardo <eduardo.oliveira@trt4.jus.br>
2021-04-26 20:36:50 -0300

    alteracao de end cors

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://pure-anchorage-24580.herokuapp.com',
   })
 );
 
