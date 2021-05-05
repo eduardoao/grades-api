25e0767b6cb858c274ae991f8cc6c19bdcaf0632
Eduardo <eduardo.oliveira@trt4.jus.br>
2021-05-05 12:28:01 -0300

    Alteracao conection string

--- models/index.js
+++ models/index.js
@@ -2,6 +2,6 @@ import mongoose from 'mongoose';
 
 const db = {};
 db.mongoose = mongoose;
-db.url = "mongodb+srv://root:Regina52@cluster0.jpzpu.mongodb.net/grades?retryWrites=true&w=majority"; //process.env.MONGODB;
+db.url = "mongodb+srv://root:05031952@cluster0.jpzpu.mongodb.net/grades?retryWrites=true&w=majority"; //process.env.MONGODB;
 
 export { db };
